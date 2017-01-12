391403faab5f851448418a4c654c2fa4a262e199
Melanie Jane Pascual <melaniejane.pascual@gmail.com>
2017-01-11 19:22:32 -0500

    update

--- _posts/2016-09-18-summer-sixteen.markdown
+++
@@ -1,9 +0,0 @@
----
-layout: post
-title:  "Summer Sixteen"
-date:   2016-08-24 
-categories: Posts
-
----
-
-After a difficult freshman year
---
+++ techpractice.java
@@ -0,0 +1,22 @@
+//Write a function that takes in an integer N and returns the sum for all numbers between 1 to N excluding multiples of 3 and 5.*/
+
+int sum(N){
+     int x = 0;
+     int sum = 0;
+     while(x <= N){
+          if((x%3 != 0) && (x%5 != 0)){
+               sum += x;
+               x++;
+          }
+     }
+     return sum;
+}
+
+
+//Given an array of  integers and a number,n,
+// perform  left rotations on the array. Then 
+//print the updated array as a single line of space-separated integers.
+
+public static int[] arrayLeftRotation(int[] a, int n, int k) {
+	
+}
